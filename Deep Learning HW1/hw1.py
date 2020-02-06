4873aa8076ff0fc2022145c47b36f2c16fd34a80
Ivan Christian <ivan_christian@mymail.sutd.edu.sg>
2020-02-06 22:20:39 +0800

    Update hw1.py

--- Deep Learning HW1/hw1.py
+++ Deep Learning HW1/hw1.py
@@ -69,16 +69,16 @@ def preprocess(dataset, reference, start=10, end=14):
 	image_label = image_label.drop(columns = [i for i in range(end,len(image_labels.columns))])
 	image_label.columns = ['image', 'spring','summer', 'autumn', 'winter']
 	image_label_spring = image_label[image_label.spring != 0]
-	spring_shape = image_label_spring.drop(columns=['image']).shape
+	spring_shape = image_label_spring.shape[0]
 	image_label_summer = image_label[image_label.summer != 0]
-	summer_shape = image_label_summer.drop(columns=['image']).shape
+	summer_shape = image_label_summer.shape[0]
 	image_label_autumn = image_label[image_label.autumn != 0]
-	autumn_shape = image_label_autumn.drop(columns=['image']).shape
+	autumn_shape = image_label_autumn.shape[0]
 	image_label_winter = image_label[image_label.winter != 0]
-	winter_shape = image_label_winter.drop(columns=['image']).shape
+	winter_shape = image_label_winter.shape[0]
 	image_label_data = pd.concat([image_label_spring, image_label_summer, image_label_autumn, image_label_winter]).drop_duplicates()
 	x = np.array([np.load(os.path.join(os.path.join(dataset_directory,"imageclef2011_feats") , file + '_ft.npy')) for file in image_label_data.image])
-	return x, image_label_data, spring_shape, summer_shape, autumn_shape, winter_shape
+	return x, image_label_data, spring_shape,summer_shape, autumn_shape, winter_shape
 '''
 function is used to split the train, test, val from the dataset
 according to the requirement: 60 (train) - 15(test) - 25 (val)
@@ -106,15 +106,12 @@ c_accuracy : 1/(total number of elements in the class)
 '''
 
 
-def class_wise_accuracy(y_pred, y):
-	c_accuracy = np.sum(y_pred == y) /np.array(y).shape
+def class_wise_accuracy(y_pred, y, shape):
+	c_accuracy = np.sum(y_pred * y) / ((shape/1353)*np.array(y).shape[0])
 	return c_accuracy
 
 '''
 function save is used to save the features in .npy files
-
-
-Change the 
 
 Input:
 
@@ -144,19 +141,21 @@ def load():
 	y_test = np.load(f'splits\\y_test.npy')
 	return x_train, x_test, x_val, y_train, y_test, y_val
 
-x, image_label_data, spring_shape, summer_shape, autumn_shape, winter_shape = preprocess(filename[1], filename[0])
+x, target, spring_shape,summer_shape, autumn_shape, winter_shape = preprocess(filename[1], filename[0])
 
 seasons =  ['spring','summer', 'autumn', 'winter']
 
+seas_dict = {seasons[0]: spring_shape, seasons[1]: summer_shape, seasons[2]: autumn_shape, seasons[3]:winter_shape}
+
 regularization_constants = [0.01, 0.1, 0.1**0.5, 1, 10**0.5, 10, 100**0.5]
-
-
 
 '''
 splitting the dataset to find the optimal c
 '''
 y = target.drop(columns=['image']) #drop the image id since its no longer needed after the preprocessing to get the relevant features
 x_train, x_test, x_val, y_train, y_test, y_val = split(x,y)
+
+
 if not os.path.exists(f'splits'):
 	os.mkdir(f'splits')
 else:
@@ -168,6 +167,7 @@ Finding optimal c
 
 for season in seasons:
 	scores = []
+
 	for c in regularization_constants:
 		svm = SVC(C=c, kernel = 'linear', probability=True)
 		svm.fit(x_train,y_train[season])
@@ -177,14 +177,14 @@ for season in seasons:
 		'''
 		y_pred_class = svm.predict(x_val)
 
-		acc = class_wise_accuracy(y_pred_class, y_val[season])
+		acc = class_wise_accuracy(y_pred_class, y_val[season], seas_dict[season])
 		scores.append(acc)
 
 	print(f'best c for {season} = {regularization_constants[np.argmax(scores)]} with score: {np.max(scores)}')
 
 
 '''
-Conclusion c should be 0.01 since it gives out maximum value
+Conclusion c should be 0.01  (based on majority since it gives out maximum value for the majority
 '''
 
 '''
@@ -203,9 +203,9 @@ for season in seasons:
 	c_concat.append(c_prelim_pred_prob[:,1])
 	c_prelim_pred = np.zeros_like(c_prelim_pred_prob)
 	c_prelim_pred[np.arange(len(c_prelim_pred_prob)), c_prelim_pred_prob.argmax(1)] = 1
-	c_acc = class_wise_accuracy(c_prelim_pred[:,1],  y_test[season])
+	c_acc = class_wise_accuracy(c_prelim_pred[:,1],  y_test[season], seas_dict[season])
 	c_averaged.append(c_acc)
-	print(f'{season} has class acc = {c_acc}')
+	# print(f'{season} has class acc = {c_acc}')
 
 '''
 Calculating the average class wise accuracy
@@ -228,7 +228,7 @@ y_prelim_pred = y_prelim_pred.flatten()
 y_test_vanilla = np.array(y_test).flatten()
 
 
-vanilla_Acc = class_wise_accuracy(y_prelim_pred, y_test_vanilla)
+vanilla_Acc = np.sum(y_prelim_pred== y_test_vanilla)/y_prelim_pred.shape[0]
 
 
 print(f'vanilla accuracy = {vanilla_Acc}')
