76a5fb6dbe1cb93a7bae3b778fd5776688e0b40a
IC088 <33095731+IC088@users.noreply.github.com>
2020-02-10 22:28:08 +0800

    Update hw2.py

--- Deep Learning HW2/hw2.py
+++ Deep Learning HW2/hw2.py
@@ -25,12 +25,15 @@ print(f'T = {T.size()}')
 
 '''
 2 loops over i,j appraoch
+
 '''
-y = 0
+d = 0
 for i in range(N):
 	for j in range(P):
-		y += X[i,:] - T[j,:]
-print(y)
+		d += (X[i,:] - T[j,:])**2
+print(d)
+	# 	print(f'T[{j}] = {T[j,:]}')
+	# print(f'X[{i}] = {X[i,:]}')
 
 
 '''
@@ -40,10 +43,31 @@ numpy broadcasting
 x = X.data.numpy()
 t = T.data.numpy()
 
-print(x,t)
+'''
+dist[i,j] = ||x[i,:]-y[j,:]||
+'''
+
+d = np.linalg.norm(x[:, np.newaxis, :] - t, axis = 2)
+print(d**2)
 
 '''
 pytorch cpu
 
 torch broadcasting
 '''
+
+def expanded_pairwise_distances(x, y):
+    '''
+    Input: x is an Nxd matrix
+           y is an Mxd matirx
+    Output: dist is a NxM matrix where dist[i,j] is the square norm between x[i,:] and y[j,:]
+    i.e. dist[i,j] = ||x[i,:]-y[j,:]||^2
+    '''
+    differences = x.unsqueeze(1) - y.unsqueeze(0)
+    distances = torch.sum(differences * differences, -1)
+    return distances
+
+l = expanded_pairwise_distances(X,T)
+print(l)
+
+
