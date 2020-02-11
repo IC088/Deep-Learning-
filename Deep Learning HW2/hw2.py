fd921b6681884e57824f6bfba42efddd7069b1fc
IC088 <33095731+IC088@users.noreply.github.com>
2020-02-11 19:19:09 +0800

    Update hw2.py

--- Deep Learning HW2/hw2.py
+++ Deep Learning HW2/hw2.py
@@ -7,14 +7,19 @@ import torch
 from random import randrange
 import numpy as np
 
+
 '''
 let the tensors be defined as the following for hte calculations
 '''
 
-N = randrange(1,10)
-D = randrange(1,10)
-P = randrange(1,10)
+N = randrange(1,100)
+D = randrange(1,100)
+P = randrange(1,100)
+'''
+TASK 1
+'''
 
+print('TASK 1 \n')
 
 X = torch.randn(N,D)
 T = torch.randn(P,D)
@@ -31,10 +36,7 @@ d = 0
 for i in range(N):
 	for j in range(P):
 		d += (X[i,:] - T[j,:])**2
-print(d)
-	# 	print(f'T[{j}] = {T[j,:]}')
-	# print(f'X[{i}] = {X[i,:]}')
-
+# print(d)
 
 '''
 numpy broadcasting
@@ -43,12 +45,10 @@ numpy broadcasting
 x = X.data.numpy()
 t = T.data.numpy()
 
-'''
-dist[i,j] = ||x[i,:]-y[j,:]||
-'''
+# dist[i,j] = ||x[i,:]-y[j,:]||
 
 d = np.linalg.norm(x[:, np.newaxis, :] - t, axis = 2)
-print(d**2)
+# print(d**2)
 
 '''
 pytorch cpu
@@ -56,7 +56,7 @@ pytorch cpu
 torch broadcasting
 '''
 
-def expanded_pairwise_distances(x, y):
+def pytorch_eucledian_distances(x, y):
     '''
     Input: x is an Nxd matrix
            y is an Mxd matirx
@@ -67,7 +67,71 @@ def expanded_pairwise_distances(x, y):
     distances = torch.sum(differences * differences, -1)
     return distances
 
-l = expanded_pairwise_distances(X,T)
-print(l)
+print('Using CPU')
+torch.device("cpu")
+l = pytorch_eucledian_distances(X,T)
+# print(l)
 
 
+
+
+device = torch.device('cuda')
+print('Using device:', torch.cuda.get_device_name(0))
+if device.type == 'cuda':
+	N = randrange(1,100)
+	D = randrange(1,100)
+	P = randrange(1,100)
+	X = torch.randn(N,D)
+	T = torch.randn(P,D)
+
+	X.to(device)
+	T.to(device)
+	l = pytorch_eucledian_distances(X,T)
+	# print(l)
+
+
+
+
+
+
+'''
+K means implementation
+
+function Kmeans is used to create teh k-means result
+
+input:
+x : data X[i,:] 5 blobs from 5 different gaussian means 
+k : Desired cluster P
+m : max number of iterations
+
+output:
+returns the 
+'''
+
+def KMeans(x, k, m):
+	'''
+	TODO: initialise cluster centers T[j,:], select 5 from x fixed
+	'''
+	
+
+	x_i = x.unsqueeze(1)
+	c = torch.gather(x, 5)
+	return c
+	'''
+	TODO: iterate the loop at most m times, stop when the 
+	'''
+	# for i in range(m):
+	# 	c_j = c.unsqueeze(0)
+	# 	d_ij = pytorch_eucledian_distances(x_i,c_j)
+
+	# 	cl = D_ij.argmin(dim=1).long().view(-1)
+
+	# 	Ncl = torch.bincount(cl).type(torchtype[dtype]) 
+ #        for d in range(D): 
+ #            c[:, d] = torch.bincount(cl, weights=x[:, d]) / Ncl
+	# return 
+
+
+
+results = KMeans(X, 5, 5)
+print(results)
