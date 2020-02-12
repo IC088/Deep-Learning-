6c94e3859ee291bd337970015abaa3a8558b49a9
IC088 <33095731+IC088@users.noreply.github.com>
2020-02-12 16:10:57 +0800

    Update hw2.py

--- Deep Learning HW2/hw2.py
+++ Deep Learning HW2/hw2.py
@@ -14,56 +14,9 @@ from time import time
 '''
 let the tensors be defined as the following for hte calculations
 '''
-
-N = randrange(1,5000)
-D = randrange(1,300)
-P = randrange(1,500)
 '''
-TASK 1
+Function pytorch_eucledian_distances is a helper function to calculate the eucledian distances 
 '''
-#
-# print('TASK 1 \n')
-#
-# X = torch.randn(N,D)
-# T = torch.randn(P,D)
-#
-#
-# print(f'X = {X.size()}')
-# print(f'T = {T.size()}')
-#
-# '''
-# 2 loops over i,j appraoch
-#
-# '''
-# a = time()
-# dist = np.zeros(N)
-# for i in range(N):
-# 	for j in range(P):
-# 		dist[i] += torch.sum(X[i,:] - T[j,:])
-# time_elapsed=float(time()) - float(a)
-# print(dist.shape)
-#
-# print(f'Time elapsed for "For loop" : {time_elapsed}')
-# '''
-# numpy broadcasting
-# '''
-#
-# x = X.data.numpy()
-# t = T.data.numpy()
-#
-# # dist[i,j] = ||x[i,:]-y[j,:]||
-# b = time()
-# d = np.linalg.norm(x[:, np.newaxis, :] - t, axis = 2)
-# time_elapsed=float(time()) - float(b)
-# print((d**2).shape)
-# print(f'Time elapsed for "Numpy broadcasting" : {time_elapsed}')
-#
-# '''
-# pytorch cpu
-#
-# torch broadcasting
-# '''
-#
 def pytorch_eucledian_distances(x, y,device=torch.device('cpu')):
     '''
     Input: x is an Nxd matrix
@@ -74,33 +27,77 @@ def pytorch_eucledian_distances(x, y,device=torch.device('cpu')):
     differences = x.unsqueeze(1) - y.unsqueeze(0)
     distances = torch.sum(differences * differences, -1)
     return distances
-# c = time()
-# print('Using CPU')
-# torch.device("cpu")
-# l = pytorch_eucledian_distances(X,T)
-# time_elapsed=float(time()) - float(c)
-# print(l.size())
-# print(f'Time elapsed for "CPU" : {time_elapsed}')
-#
-#
-#
-#
-#
-# device = torch.device( cuda if torch.cuda.is_available() else 'cpu' )
-# if device.type == 'cuda':
-# 	X = torch.randn(N,D)
-# 	T = torch.randn(P,D)
-# 	X.to(device)
-# 	T.to(device)
-# 	d = time()
-# 	l = pytorch_eucledian_distances(X,T, device)
-# 	time_elapsed=float(time()) - float(d)
-# 	print(l.size())
-# 	print(f'Time elapsed for "GPU" : {time_elapsed}')
+
+N = randrange(1,5000)
+D = randrange(1,300)
+P = randrange(1,500)
+'''
+TASK 1
+'''
+
+print('TASK 1 \n')
+
+X = torch.randn(N,D)
+T = torch.randn(P,D)
+
+
+print(f'X = {X.size()}')
+print(f'T = {T.size()}')
+
+'''
+2 loops over i,j appraoch
+
+'''
+a = time()
+dist = np.zeros(N)
+for i in range(N):
+	for j in range(P):
+		dist[i] += torch.sum(X[i,:] - T[j,:])
+time_elapsed=float(time()) - float(a)
+print(dist.shape)
+
+print(f'Time elapsed for "For loop" : {time_elapsed}')
+'''
+numpy broadcasting
+'''
+
+x = X.data.numpy()
+t = T.data.numpy()
+
+# dist[i,j] = ||x[i,:]-y[j,:]||
+b = time()
+d = np.linalg.norm(x[:, np.newaxis, :] - t, axis = 2)
+time_elapsed=float(time()) - float(b)
+print((d**2).shape)
+print(f'Time elapsed for "Numpy broadcasting" : {time_elapsed}')
+
+'''
+pytorch cpu
+
+torch broadcasting
+'''
+
+c = time()
+print('Using CPU')
+torch.device("cpu")
+l = pytorch_eucledian_distances(X,T)
+time_elapsed=float(time()) - float(c)
+print(l.size())
+print(f'Time elapsed for "CPU" : {time_elapsed}')
 
 
 
-
+device = torch.device( 'cuda' if torch.cuda.is_available() else 'cpu' )
+if device.type == 'cuda':
+	X = torch.randn(N,D)
+	T = torch.randn(P,D)
+	X.to(device)
+	T.to(device)
+	d = time()
+	l = pytorch_eucledian_distances(X,T, device)
+	time_elapsed=float(time()) - float(d)
+	print(l.size())
+	print(f'Time elapsed for "GPU" : {time_elapsed}')
 
 '''
 K means implementation
@@ -119,15 +116,13 @@ choice_clusters : the classification cluster for the points
 init_centroid_i : final centroids that classifies the data points
 '''
 
-
-
 def KMeans(x, k, m, device=torch.device('cpu')):
 	'''
 	TODO: initialise cluster centers T[j,:], select 5 from x fixed
 	'''
 	init_centroid = initialise(x,k)
-	x_i = torch.from_numpy(x).float().to(device)
-	init_centroid_i = torch.from_numpy(init_centroid).float().to(device)
+	x_i = torch.from_numpy(x).float().to(device).cpu()
+	init_centroid_i = torch.from_numpy(init_centroid).float().to(device).cpu()
 	iterations = 0
 
 
@@ -138,13 +133,15 @@ def KMeans(x, k, m, device=torch.device('cpu')):
 
 		initial_state_pre = init_centroid_i.clone()
 		for index in range(k):
-			selected = torch.nonzero(choice_cluster == index).squeeze().to(device)
-			selected = torch.index_select(x_i, 0, selected)
+			selected = torch.nonzero(choice_cluster == index).squeeze().to(device).cpu()
+			selected = torch.index_select(x_i, 0, selected).to(device).cpu()
 			init_centroid_i[index] = selected.mean(dim=0)
-		print(init_centroid_i - initial_state_pre)
-		# center_shift = torch.sum(torch.sqrt(torch.sum((init_centroid_i - initial_state_pre) ** 2, dim=1)))
-		# center_shift = pytorch_eucledian_distances(init_centroid_i, initial_state_pre,device=device)
-
+		
+		center_shift = torch.sum(torch.sqrt(torch.sum((init_centroid_i - initial_state_pre) ** 2, dim=1)))
+		if center_shift < 0.001:
+			print(center_shift)
+			print(iterations)
+			break
 		iterations+=1
 	# print(center_shift)
 	return choice_cluster, init_centroid_i
@@ -167,7 +164,7 @@ init_centroid : array of initial centroids.
 def initialise(x,k=5):
 	num_samples = len(x)
 	'''
-	TODO: fixed choices for X
+	fixed choices for X
 	'''
 	idx = [i for i in range(k)]
 	indices = tuple(idx)
@@ -180,13 +177,13 @@ dataset = []
 for i in range(5):
 	mean = randrange(-10,10)
 	sigma = randrange(1,2)
-	dataset1 = np.random.normal(mean,sigma,(50,2))
+	dataset1 = np.random.normal(mean,sigma,(10000,2))
 	dataset.append(dataset1)
 
 X = np.vstack(dataset)
 X = np.take(X,np.random.permutation(X.shape[0]),axis=0,out=X)
 
-choice_cluster, centroid= KMeans(X, 8, 500)
+choice_cluster, centroid= KMeans(X, 8, 10000, device=torch.device( 'cuda' if torch.cuda.is_available() else 'cpu' ))
 # print(choice_cluster, centroid)
 plt.scatter(X[:, 0], X[:, 1], c=choice_cluster)
 plt.scatter(centroid[:,0], centroid[:,1], c ='black')
