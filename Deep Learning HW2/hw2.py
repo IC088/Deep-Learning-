ec183566e52401bb0f697ea2b6ac423016f76348
IC088 <33095731+IC088@users.noreply.github.com>
2020-02-12 19:46:28 +0800

    Updated

--- Deep Learning HW2/hw2.py
+++ Deep Learning HW2/hw2.py
@@ -30,9 +30,10 @@ def pytorch_eucledian_distances(x, y,device=torch.device('cpu')):
     distances = torch.sum(differences * differences, -1)
     return distances
 
-N = randrange(1,5000)
-D = randrange(1,300)
-P = randrange(1,500)
+N = randrange(1,500)
+D = randrange(1,200)
+P = randrange(1,300)
+
 '''
 TASK 1
 '''
@@ -51,12 +52,13 @@ print(f'T = {T.size()}')
 
 '''
 a = time()
-dist = np.zeros(N)
+dist = np.zeros((N,P))
 for i in range(N):
 	for j in range(P):
-		dist[i] += torch.sum((X[i,:] - T[j,:])**2, dim = -1)
-time_elapsed=float(time()) - float(a)
+		y = torch.sum((X[i,:] - T[j,:])**2,dim=0)
+		dist[i][j] = y
 print(dist.shape)
+time_elapsed=float(time()) - float(a)
 
 print(f'Time elapsed for "For loop" : {time_elapsed}')
 '''
@@ -101,108 +103,109 @@ if device.type == 'cuda':
 	print(l.size())
 	print(f'Time elapsed for "GPU" : {time_elapsed}')
 
-# '''
-# K means implementation
+'''
+K means implementation
 
-# function Kmeans is used to create teh k-means result
+function Kmeans is used to create teh k-means result
 
-# input:
-# x : data X[i,:] 5 blobs from 5 different gaussian means
-# k : Desired number of clusters P
-# m : max number of iterations
-# device : choose which device to use, defaulteed to cpu
+input:
+x : data X[i,:] 5 blobs from 5 different gaussian means
+k : Desired number of clusters P
+m : max number of iterations
+device : choose which device to use, defaulteed to cpu
 
-# output:
-# returns
-# choice_clusters : the classification cluster for the points
-# init_centroid_i : final centroids that classifies the data points
-# '''
+output:
+returns
+choice_clusters : the classification cluster for the points
+init_centroid_i : final centroids that classifies the data points
+'''
 
-# def KMeans(x, m, k=5, device=torch.device('cpu')):
-# 	'''
-# 	TODO: initialise cluster centers T[j,:], select 5 from x fixed
-# 	'''
-# 	init_centroid = initialise(x,k)
-# 	x_i = torch.from_numpy(x).float().to(device).cpu()
-# 	init_centroid_i = torch.from_numpy(init_centroid).float().to(device).cpu()
-# 	iterations = 0
+def KMeans(x, m, k=5, device=torch.device('cpu')):
+	'''
+	TODO: initialise cluster centers T[j,:], select 5 from x fixed
+	'''
+	init_centroid = initialise(x,k)
+	x_i = torch.from_numpy(x).float().to(device).cpu()
+	init_centroid_i = torch.from_numpy(init_centroid).float().to(device).cpu()
+	iterations = 0
 
 
-# 	while iterations < m:
-# 		dist = pytorch_eucledian_distances(x_i, init_centroid_i,device=device)
-# 		choice_cluster = torch.argmin(dist, dim=1)
-# 		# print(choice_cluster)
+	while iterations < m:
+		dist = pytorch_eucledian_distances(x_i, init_centroid_i,device=device)
+		choice_cluster = torch.argmin(dist, dim=1)
+		# print(choice_cluster)
 
-# 		initial_state_pre = init_centroid_i.clone()
-# 		for index in range(k):
-# 			selected = torch.nonzero(choice_cluster == index).squeeze().to(device).cpu()
-# 			selected = torch.index_select(x_i, 0, selected).to(device).cpu()
-# 			init_centroid_i[index] = selected.mean(dim=0)
+		initial_state_pre = init_centroid_i.clone()
+		for index in range(k):
+			selected = torch.nonzero(choice_cluster == index).squeeze().to(device).cpu()
+			selected = torch.index_select(x_i, 0, selected).to(device).cpu()
+			init_centroid_i[index] = selected.mean(dim=0)
 		
-# 		loss = torch.sum(torch.sqrt(torch.sum((init_centroid_i - initial_state_pre) ** 2, dim=1)))
-# 		if loss < 0.01:
-# 			print(loss)
-# 			print(f'Number of iterations taken: {iterations}')
-# 			break
-# 		iterations+=1
+		loss = torch.sum(torch.sqrt(torch.sum((init_centroid_i - initial_state_pre) ** 2, dim=1)))
+		if loss < 0.01:
+			print(loss)
+			print(f'Number of iterations taken: {iterations}')
+			break
+		iterations+=1
 
-# 	# print(center_shift)
-# 	return choice_cluster, init_centroid_i
+	# print(center_shift)
+	return choice_cluster, init_centroid_i
 
-# '''
-# Function initialise is a helper function in doing the k-means.
-# The use of this function is to choose the initial k centroids for the k-means process.
-# The first k elements of x will be used as the initial centroids for the process
+'''
+Function initialise is a helper function in doing the k-means.
+The use of this function is to choose the initial k centroids for the k-means process.
+The first k elements of x will be used as the initial centroids for the process
 
-# input :
-# x : numpy array of the dataset. This will be used to determine the initial centroids that is chosenself.
-# k : integer input to determine how many elements are going to become the initial centroidsself.
-# k is defaulted to 5, but Homework mentions that k can be 2,5,8
+input :
+x : numpy array of the dataset. This will be used to determine the initial centroids that is chosenself.
+k : integer input to determine how many elements are going to become the initial centroidsself.
+k is defaulted to 5, but Homework mentions that k can be 2,5,8
 
-# output:
+output:
 
-# init_centroid : array of initial centroids.
+init_centroid : array of initial centroids.
 
-# '''
+'''
 
-# def initialise(x,k):
-# 	num_samples = len(x)
-# 	'''
-# 	fixed choices for X
-# 	'''
-# 	idx = [i for i in range(k)]
-# 	indices = tuple(idx)
-# 	init_centroid = x[indices,:]
+def initialise(x,k):
+	num_samples = len(x)
+	'''
+	fixed choices for X
+	'''
+	idx = [i for i in range(k)]
+	indices = tuple(idx)
+	init_centroid = x[indices,:]
 
-# 	return init_centroid
+	return init_centroid
 
-# print('TASK 2: K-Means  with pytorch')
+print('TASK 2: K-Means  with pytorch')
 
 
-# if os.path.exists(os.path.join('.','data.npy')):
-# 	print('Dataset exists, loading dataset')
-# 	X = np.load(os.path.join('.','data.npy'))
-# else:
-# 	# Creating a fixed dataset so that the TAs can replicate the 
-# 	d1 = (-10, 2) 
-# 	d2 = (-5, 2)
-# 	d3 = (0, 2)
-# 	d4 = (5, 2)
-# 	d5 = (10, 2)
+if os.path.exists(os.path.join('.','data.npy')):
+	print('Dataset exists, loading dataset')
+	X = np.load(os.path.join('.','data.npy'))
+else:
+	# Creating a fixed dataset so that the TAs can replicate the 
+	d1 = (-10, 2) 
+	d2 = (-5, 2)
+	d3 = (0, 2)
+	d4 = (5, 2)
+	d5 = (10, 2)
 
-# 	mean_sigma = [d1,d2,d3,d4,d5]
-# 	dataset = []
+	mean_sigma = [d1,d2,d3,d4,d5]
+	dataset = []
 
-# 	for mean, sigma in mean_sigma:
-# 		dset = np.random.normal(mean,sigma,(5000,2))
-# 		dataset.append(dset)
+	for mean, sigma in mean_sigma:
+		dset = np.random.normal(mean,sigma,(5000,2))
+		dataset.append(dset)
 
-# 	X = np.vstack(dataset)
-# 	X = np.take(X,np.random.permutation(X.shape[0]),axis=0,out=X)
-# 	print('Creating dataset')
-# 	np.save(os.path.join('.','data.npy'), X)
+	X = np.vstack(dataset)
+	X = np.take(X,np.random.permutation(X.shape[0]),axis=0,out=X)
+	print('Creating dataset')
+	np.save(os.path.join('.','data.npy'), X)
 
-# choice_cluster, centroid= KMeans(X, 10000, k=5, device=torch.device( 'cuda' if torch.cuda.is_available() else 'cpu' ))
-# plt.scatter(X[:, 0], X[:, 1], c=choice_cluster)
-# plt.scatter(centroid[:,0], centroid[:,1], c ='black')
-# plt.show()
+choice_cluster, centroid= KMeans(X, 10000, k=8, device=torch.device( 'cuda' if torch.cuda.is_available() else 'cpu' ))
+plt.scatter(X[:, 0], X[:, 1], c=choice_cluster)
+plt.scatter(centroid[:,0], centroid[:,1], c ='black')
+plt.savefig(os.path.join('.',f'figure-{8}.png'))
+plt.show()
