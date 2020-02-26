c988b2466f7aa2f5ed5994bddccc6e464def6ce4
IC088 <33095731+IC088@users.noreply.github.com>
2020-02-27 00:26:30 +0800

    update

--- Deep_Learning_HW4/Part_2/hw4_task2.py
+++ Deep_Learning_HW4/Part_2/hw4_task2.py
@@ -20,16 +20,16 @@ def create_loaders(im_dir, im_paths, label, train_split=0.7, val_split=0.1):
 	Function create_loaders is a function to create data loader for training
 
 	Input:
-	- im_dir
-	- im_paths
-	- label
-	- train_split
-	- val_split
+	- im_dir : string
+	- im_paths : string
+	- label : 
+	- train_split : float ()
+	- val_split : float ()
 
 	Output:
-	- train_loader
-	- val_loader
-	- test_loader
+	- train_loader : DataLoader for training 
+	- val_loader : DataLoader for validation
+	- test_loader : DataLoader for testing
 	'''
 
 	transformation = transforms.Compose([transforms.CenterCrop(224), transforms.ToTensor()])
@@ -67,6 +67,8 @@ def train( model, device , train_loader , optimizer, epochs, loss_func ):
 
 	train_losses = []
 
+	correct = 0
+
 	for index, batch in enumerate(train_loader):
 		data = batch['image'].to(device)
 		target = batch['label'].long().to(device)
@@ -75,10 +77,15 @@ def train( model, device , train_loader , optimizer, epochs, loss_func ):
 		loss = loss_func(output, target)#CrossEntropyLoss
 		loss.backward() #calculate the gradient 
 		optimizer.step()
+
+
+		# correct += (output == target).float().sum()
+
+
 		train_losses.append(loss.item())
 	train_loss = torch.mean((torch.tensor(train_losses)))
 
-	return train_loss
+	return train_loss, train_accuracy
 
 def validate ( model , device , val_loader , loss_func):
 	'''
@@ -134,6 +141,55 @@ def test (model, device, test_loader):
 	accuracy /= len(test_loader.dataset)
 	return accuracy 
 
+
+def custom_training(im_dir, im_paths, label, mode = 1, epochs = 10):
+
+	device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+	if mode == 1:
+		# mode 1 is to train from scracth
+		model = models.resnet18(num_classes=102).to(device)
+	elif mode == 2:
+		# mode 2 is to do training all layers
+		model = models.resnet18(pretrained=True).to(device)
+	elif model == 3:
+		# mode 3 is to do pre trained weights
+		model = models.resnet18(pretrained=True).to(device)
+		model.fc = nn.Linear(512, 102)
+
+	else:
+		print('Mode not recognised')
+		return 
+
+	optimizer = optim.SGD(model.parameters(), lr=1e-3, momentum=0.9)
+
+	loss_func = nn.CrossEntropyLoss()
+
+	train_loader, val_loader, test_loader = create_loaders(im_dir, im_paths, label)
+
+
+	train_losses_vis = []
+	val_losses_vis = []
+
+	for epoch in range(1, epochs + 1):
+		train_loss = train(model, device, train_loader, optimizer, epoch)
+		val_loss, val_accuracy = validate(model, device, val_loader)
+
+		if (len(val_losses) > 0) and (val_loss < min(val_losses)):
+			torch.save(model.state_dict(), "best_model.pt")
+		train_losses.append(train_loss)
+    	val_losses.append(val_loss)
+	
+	print('Finished Training and Validation Process')
+
+	test_accuracy = 
+
+	if mode == 1:
+		visualise_train_val_loss(train_losses_vis, val_losses_vis, epochs, 'custom_full_training.png' )
+	elif mode == 2:
+		visualise_train_val_loss(train_losses_vis, val_losses_vis, epochs, 'custom_last2_layers.png' )
+	elif model == 3:
+		visualise_train_val_loss(train_losses_vis, val_losses_vis, epochs, 'custom_pre_trained.png' )
 
 def run():
 	pass
